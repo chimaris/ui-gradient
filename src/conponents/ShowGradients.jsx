eca9edd64af0757ed0e849286414853c669ab9c1
Stellamaris Nwafor <wendumaris@gmail.com>
2022-12-08 02:29:01 +0100

    Worked on download gradient as image, responsiveness and select gradient from show all gradients

--- src/conponents/ShowGradients.jsx
+++ src/conponents/ShowGradients.jsx
@@ -2,26 +2,24 @@ import { Text, Grid, GridItem } from '@chakra-ui/react'
 
 import classes from './ShowGradients.module.css'
 
-const GradientBox = ({ gradient }) => {
-    const ani = {
+const GradientBox = ({ gradient, id, showCurrentGrad }) => {
 
-    }
     const container = {
         bgGradient: `linear(to-r, ${gradient.color1}, ${gradient.color2})`
     }
     return (
-        <GridItem sx={container} h={150} width='100%'>
+        <GridItem sx={container} h={150} width='100%' onClick={() => showCurrentGrad(id)} className={classes.grid}>
             <Text color='white' my={10}>{gradient.name}</Text>
         </GridItem>
     )
 }
 
-const ShowGradients = ({ gradients }) => {
+const ShowGradients = ({ gradients, showCurrentGrad }) => {
 
     return (
-        <Grid templateColumns='repeat(3, 1fr)' gap={4} className={classes.ani}>
+        <Grid templateColumns='repeat(3, 1fr)' gap={4} className={classes.gridBox}>
             {gradients.map((gradient, index) => (
-                <GradientBox key={index} gradient={gradient} />
+                <GradientBox key={index} id={index} gradient={gradient} showCurrentGrad={showCurrentGrad} />
             ))}
         </Grid>
     )
